97192435504603afe9007b1ce3ab003c84559dbe
Chris Wirth <cwirth9@gmail.com>
2022-02-01 16:06:24 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// Calculation Functions
+class Calculator {
+  add(num1, num2) {
+    return num1 + num2;
+  }
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+  divide(num1, num2) {
+    return num1 / num2;
+  }
+}
+
+// Persistent Variables
+const calc = new Calculator();
+const result = document.querySelector('.result');
+const clear = document.querySelector('.clear');
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+// Changing Variables
+let num1;
+let num2;
+let operator;
+
+// Clear
+clear.addEventListener('click', () => {
+  num1 = undefined;
+  num2 = undefined;
+  result.textContent = '0';
+});
+
+// Operation
+function operatorClicked() {
+  operator = this.classList[1];
+}
+
+operators.forEach(operator => {
+  operator.addEventListener('click', (operatorClicked));
+});
+
+// Number
+function numberClicked() {
+  const number = this.textContent;
+  result.textContent = number;
+  if (!num1) {
+    num1 = parseInt(number);
+  } else {
+    num2 = parseInt(number);
+  }
+}
+
+numbers.forEach(number => {
+  number.addEventListener('click', numberClicked);
+});
+
+// Calculate Result
+equals.addEventListener('click', () => {
+  result.textContent = calc[operator](num1, num2).toString();
+});
